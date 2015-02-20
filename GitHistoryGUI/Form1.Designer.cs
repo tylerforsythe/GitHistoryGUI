d66407cdd8071fbbe8e12a9c3da8358acffc293a
Tyler Forsythe <forsythe.tyler@gmail.com>
2015-02-19 19:07:43 -0700

    Initial cut. Pretty much works but the users are not yet taken into account. Also, the working directory path is hard-coded. So, needs a bit of config work, but functionally it is all there.

---
+++ GitHistoryGUI/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace GitHistoryGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.dtPicker = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtUsersCsv = new System.Windows.Forms.TextBox();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // dtPicker
+            // 
+            this.dtPicker.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtPicker.Location = new System.Drawing.Point(52, 15);
+            this.dtPicker.Name = "dtPicker";
+            this.dtPicker.Size = new System.Drawing.Size(105, 20);
+            this.dtPicker.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Date:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(184, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Users (CSV):";
+            // 
+            // txtUsersCsv
+            // 
+            this.txtUsersCsv.Location = new System.Drawing.Point(257, 15);
+            this.txtUsersCsv.Name = "txtUsersCsv";
+            this.txtUsersCsv.Size = new System.Drawing.Size(259, 20);
+            this.txtUsersCsv.TabIndex = 1;
+            this.txtUsersCsv.TextChanged += new System.EventHandler(this.txtUsersCsv_TextChanged);
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(12, 79);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(823, 500);
+            this.txtResult.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 60);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "History Result:";
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(760, 13);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(75, 23);
+            this.btnGo.TabIndex = 2;
+            this.btnGo.Text = "Go";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(847, 591);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.txtUsersCsv);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dtPicker);
+            this.Name = "Form1";
+            this.Text = "Git History GUI";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dtPicker;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtUsersCsv;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnGo;
+    }
+}
+
---
+++ GitHistoryGUI/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GitHistoryGUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1() {
+            InitializeComponent();
+
+            txtUsersCsv.Text = "tylerforsythe,Tyler Forsythe";
+        }
+
+        private void btnGo_Click(object sender, EventArgs e) {
+            var commandString = GetCommandString();
+            var args = GetArgumentsString();
+
+            var process = new Process();
+            process.StartInfo.WorkingDirectory = @"C:\home\projects\IMPrinting\src-lucy";
+            process.StartInfo.FileName = commandString;
+            process.StartInfo.Arguments = args;
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.Start();
+            string output = process.StandardOutput.ReadToEnd();
+            process.WaitForExit();
+
+            output = output.Replace("\n", Environment.NewLine);
+            output = output.Replace(Environment.NewLine + Environment.NewLine, Environment.NewLine);
+
+            txtResult.Text = output;
+        }
+
+        private string GetCommandString() {
+            return string.Format("git");
+        }
+
+        private string GetArgumentsString() {
+            return string.Format(" log --reverse --author=\"tylerforsythe\" --author=\"Tyler Forsythe\" --after=\"{0} 00:00:00\" --before=\"{0} 23:59:59\" --format=%B",
+                FormatDateForGit(dtPicker.Value.Date));
+        }
+
+        private string FormatDateForGit(DateTime dt) {
+            return string.Format("{0}-{1}-{2}", dt.Year, dt.Month.ToString("D2"), dt.Day.ToString("D2"));
+        }
+
+        private void txtUsersCsv_TextChanged(object sender, EventArgs e) {
+
+        }
+    }
+}
